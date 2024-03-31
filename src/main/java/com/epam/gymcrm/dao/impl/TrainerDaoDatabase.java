2d290958f46faeb977f4df15a702bcdff805ac1e
kol2104 <kni2104kni@mail.ru>
2024-04-01 01:41:01 +0500

    Update trainer dao layer

--- src/main/java/com/epam/gymcrm/dao/impl/TrainerDaoDatabase.java
+++ src/main/java/com/epam/gymcrm/dao/impl/TrainerDaoDatabase.java
@@ -1,48 +1,22 @@
 package com.epam.gymcrm.dao.impl;
 
-import com.epam.gymcrm.config.property.YamlPropertySourceFactory;
 import com.epam.gymcrm.dao.TrainerDao;
 import com.epam.gymcrm.model.Trainer;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
+import jakarta.persistence.PersistenceContext;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.ResourceUtils;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
-@PropertySource(value = "classpath:application.yml", factory = YamlPropertySourceFactory.class)
 public class TrainerDaoDatabase implements TrainerDao {
 
-    @Value("${data.json.path.trainers}")
-    private String dataJsonFilePath;
-
-    private final ObjectMapper objectMapper;
-    private final EntityManager entityManager;
-
-    @PostConstruct
-    private void init() {
-        try {
-            File jsonFile = ResourceUtils.getFile("classpath:" + dataJsonFilePath);
-            List<Trainer> preparedData = objectMapper.readValue(jsonFile, new TypeReference<>() {});
-            preparedData.forEach(this::create);
-            log.info("Initialization of TrainerDaoDatabase completed successfully.");
-        } catch (IOException e) {
-            log.error("Error occurred during initialization of TrainerDaoDatabase: {}", e.getMessage());
-        }
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Transactional
     @Override
@@ -72,6 +46,37 @@ public class TrainerDaoDatabase implements TrainerDao {
         return Optional.ofNullable(trainer);
     }
 
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<Trainer> getByUsername(String username) {
+        Optional<Trainer> foundTrainer = entityManager
+                .createQuery("from Trainer t where t.username = :username", Trainer.class)
+                .setParameter("username", username)
+                .getResultStream().findFirst();
+        if (foundTrainer.isPresent()) {
+            log.debug("Found trainer by username '{}'", username);
+        } else {
+            log.debug("Trainer with username '{}'.", username);
+        }
+        return foundTrainer;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<Trainer> getByUsernameAndPassword(String username, String password) {
+        Optional<Trainer> foundTrainer = entityManager
+                .createQuery("from Trainer t where t.username = :username and t.password = :password", Trainer.class)
+                .setParameter("username", username)
+                .setParameter("password", password)
+                .getResultStream().findFirst();
+        if (foundTrainer.isPresent()) {
+            log.debug("Found trainer by username '{}' and password '{}'", username, password);
+        } else {
+            log.debug("Trainer with username '{}' and password '{}' not found.", username, password);
+        }
+        return foundTrainer;
+    }
+
     @Transactional
     @Override
     public Trainer update(Trainer trainer) {
@@ -80,18 +85,18 @@ public class TrainerDaoDatabase implements TrainerDao {
             return null;
         }
         entityManager.merge(trainer);
-        log.info("Trainee updated successfully: {}", trainer);
+        log.info("Trainer updated successfully: {}", trainer);
         return trainer;
     }
 
     @Transactional(readOnly = true)
     @Override
     public Optional<Trainer> getByFirstNameAndLastName(String firstName, String lastName) {
-        Optional<Trainer> foundTrainer = Optional.ofNullable(entityManager
+        Optional<Trainer> foundTrainer = entityManager
                 .createQuery("from Trainer t where t.firstName = :firstName and t.lastName = :lastName", Trainer.class)
                 .setParameter("firstName", firstName)
                 .setParameter("lastName", lastName)
-                .getSingleResult());
+                .getResultStream().findFirst();
         if (foundTrainer.isPresent()) {
             log.debug("Found trainer by first name '{}' and last name '{}': {}", firstName, lastName, foundTrainer.get());
         } else {
