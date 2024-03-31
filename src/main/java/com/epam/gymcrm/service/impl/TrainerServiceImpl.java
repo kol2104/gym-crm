23d98effb16b7ded519fe7adabe15a8f6529c262
kol2104 <kni2104kni@mail.ru>
2024-04-01 01:42:46 +0500

    Update service layer

--- src/main/java/com/epam/gymcrm/service/impl/TrainerServiceImpl.java
+++ src/main/java/com/epam/gymcrm/service/impl/TrainerServiceImpl.java
@@ -1,30 +1,31 @@
 package com.epam.gymcrm.service.impl;
 
 import com.epam.gymcrm.dao.TrainerDao;
+import com.epam.gymcrm.exception.TraineeNotFoundException;
 import com.epam.gymcrm.exception.TrainerNotFoundException;
+import com.epam.gymcrm.model.Trainee;
 import com.epam.gymcrm.model.Trainer;
 import com.epam.gymcrm.service.TrainerService;
 import com.epam.gymcrm.util.PasswordUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class TrainerServiceImpl implements TrainerService {
-    @Autowired
-    private TrainerDao trainerDao;
+
+    private final TrainerDao trainerDao;
 
     @Override
     public Trainer create(Trainer trainer) {
         log.info("Saving trainer: {}", trainer);
-        String username = trainer.getFirstName() + "." + trainer.getLastName();
-        trainer.setUsername(username);
-        trainerDao.getByFirstNameAndLastName(trainer.getFirstName(), trainer.getLastName())
-                .ifPresent(t -> trainer.setUsername(username + trainer.getId()));
-
+        trainer.setUsername(getUsername(trainer.getFirstName(), trainer.getLastName()));
         trainer.setPassword(PasswordUtil.getRandomPassword(10));
         return trainerDao.create(trainer);
     }
@@ -46,13 +47,86 @@ public class TrainerServiceImpl implements TrainerService {
     }
 
     @Override
+    public Trainer getByUsername(String username) {
+        log.info("Finding trainer by username: {}", username);
+        return trainerDao.getByUsername(username)
+                .orElseThrow(() -> {
+                    log.error("Trainer with username {} not found.", username);
+                    return new TrainerNotFoundException(username);
+                });
+    }
+
+    @Override
+    public boolean isUsernameAndPasswordValid(String username, String password) {
+        log.info("Finding trainer by username '{}' and password", username);
+        return trainerDao.getByUsernameAndPassword(username, password).isPresent();
+    }
+
+    @Override
     public Trainer update(Long id, Trainer trainer) {
         log.info("Updating trainer with id {}: {}", id, trainer);
-        if (trainerDao.getById(id).isEmpty()) {
+        Optional<Trainer> trainerFromDatabase = trainerDao.getById(id);
+        if (trainerFromDatabase.isEmpty()) {
             log.error("Trainer with id {} not found.", id);
             throw new TrainerNotFoundException(id);
         }
         trainer.setId(id);
+        if (trainer.getUsername() == null) {
+            trainer.setUsername(getUsername(trainer.getFirstName(), trainer.getLastName()));
+        }
+        trainer.setPassword(trainerFromDatabase.get().getPassword());
         return trainerDao.update(trainer);
+    }
+
+    @Override
+    public void updatePassword(Long id, String newPassword) {
+        log.info("Updating trainer's password with id {}", id);
+        if (newPassword == null) {
+            log.warn("Passed new password is null");
+        }
+        Trainer foundTrainee = trainerDao.getById(id).orElseThrow(() -> {
+            log.error("Trainer with id {} not found.", id);
+            return new TrainerNotFoundException(id);
+        });
+        foundTrainee.setPassword(newPassword);
+        trainerDao.update(foundTrainee);
+    }
+
+    @Override
+    public void activate(Long id) {
+        log.info("Activating trainer with id {}", id);
+        Trainer foundTrainee = trainerDao.getById(id).orElseThrow(() -> {
+            log.error("Trainer with id {} not found.", id);
+            return new TrainerNotFoundException(id);
+        });
+        if (foundTrainee.isActive()) {
+            log.debug("Trainer already is activated");
+            return;
+        }
+        foundTrainee.setActive(true);
+        trainerDao.update(foundTrainee);
+    }
+
+    @Override
+    public void deactivate(Long id) {
+        log.info("Deactivating trainer with id {}", id);
+        Trainer foundTrainee = trainerDao.getById(id).orElseThrow(() -> {
+            log.error("Trainer with id {} not found.", id);
+            return new TrainerNotFoundException(id);
+        });
+        if (!foundTrainee.isActive()) {
+            log.debug("Trainer already is deactivated");
+            return;
+        }
+        foundTrainee.setActive(false);
+        trainerDao.update(foundTrainee);
+    }
+
+    private String getUsername(String firstName, String lastName) {
+        String username = firstName + "." + lastName;
+        if (trainerDao.getByFirstNameAndLastName(firstName, lastName).isPresent()) {
+            return username + "_" + System.currentTimeMillis();
+        }
+        return username;
     }
 }
--- src/main/java/com/epam/gymcrm/service/impl/TrainingServiceImpl.java
+++ src/main/java/com/epam/gymcrm/service/impl/TrainingServiceImpl.java
@@ -7,32 +7,38 @@ import com.epam.gymcrm.exception.TraineeNotFoundException;
 import com.epam.gymcrm.exception.TrainerNotFoundException;
 import com.epam.gymcrm.exception.TrainingNotFoundException;
 import com.epam.gymcrm.model.Training;
+import com.epam.gymcrm.model.TrainingCriteria;
 import com.epam.gymcrm.service.TrainingService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class TrainingServiceImpl implements TrainingService {
-    @Autowired
-    private TrainingDao trainingDao;
-    @Autowired
-    private TrainerDao trainerDao;
-    @Autowired
-    private TraineeDao traineeDao;
+
+    private final TrainingDao trainingDao;
+
+    private final TrainerDao trainerDao;
+
+    private final TraineeDao traineeDao;
 
     @Override
     public Training create(Training training) {
-        if (traineeDao.getById(training.getTraineeId()).isEmpty()) {
-            log.error("Trainee with id {} not found.", training.getTraineeId());
-            throw new TraineeNotFoundException(training.getTraineeId());
+        if (traineeDao.getById(training.getTrainee().getId()).isEmpty()) {
+            log.error("Trainee with id {} not found.", training.getTrainee().getId());
+            throw new TraineeNotFoundException(training.getTrainee().getId());
         }
-        if (trainerDao.getById(training.getTrainerId()).isEmpty()) {
-            log.error("Trainer with id {} not found.", training.getTrainerId());
-            throw new TrainerNotFoundException(training.getTrainerId());
+        if (trainerDao.getById(training.getTrainer().getId()).isEmpty()) {
+            log.error("Trainer with id {} not found.", training.getTrainer().getId());
+            throw new TrainerNotFoundException(training.getTrainer().getId());
         }
         log.info("Saving training: {}", training);
         return trainingDao.create(training);
@@ -52,5 +58,26 @@ public class TrainingServiceImpl implements TrainingService {
                     log.error("Training with id {} not found.", id);
                     return new TrainingNotFoundException(id);
                 });
+    }
+
+    @Override
+    public List<Training> getTrainingByTraineeUsernameAndCriteria(String username, Map<TrainingCriteria, Object> criteria) {
+        log.info("Finding trainings by trainee username '{}' and criteria", username);
+        if (criteria == null) {
+            criteria = new EnumMap<>(TrainingCriteria.class);
+        }
+        criteria.put(TrainingCriteria.TRAINEE_USERNAME, username);
+        return trainingDao.getTrainingsByCriteria(criteria);
+    }
+
+    @Override
+    public List<Training> getTrainingByTrainerUsernameAndCriteria(String username, Map<TrainingCriteria, Object> criteria) {
+        log.info("Finding trainings by trainer username '{}' and criteria", username);
+
+        if (criteria == null) {
+            criteria = new EnumMap<>(TrainingCriteria.class);
+        }
+        criteria.put(TrainingCriteria.TRAINER_USERNAME, username);
+        return trainingDao.getTrainingsByCriteria(criteria);
     }
 }
