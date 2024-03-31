0b7d2760d3c7acf457894235975c5c41e2025877
kol2104 <kni2104kni@mail.ru>
2024-04-01 01:42:17 +0500

    Update service interfaces

--- src/main/java/com/epam/gymcrm/service/TraineeService.java
+++ src/main/java/com/epam/gymcrm/service/TraineeService.java
@@ -1,6 +1,7 @@
 package com.epam.gymcrm.service;
 
 import com.epam.gymcrm.model.Trainee;
+import com.epam.gymcrm.model.Trainer;
 
 import java.util.List;
 
@@ -8,7 +9,15 @@ public interface TraineeService {
     Trainee create(Trainee trainee);
     List<Trainee> getAll();
     Trainee getById(Long id);
+    Trainee getByUsername(String username);
+    boolean isUsernameAndPasswordValid(String username, String password);
     void delete(Long id);
+    void deleteByUsername(String username);
     Trainee update(Long id, Trainee trainee);
+    void updatePassword(Long id, String newPassword);
+    void updateTrainersList(Long id, List<Trainer> trainers);
+    List<Trainer> getUnassignedOnTraineeTrainerListByUsername(String username);
+    void activate(Long id);
+    void deactivate(Long id);
 
 }
