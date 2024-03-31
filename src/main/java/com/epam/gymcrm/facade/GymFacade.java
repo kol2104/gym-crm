2603fba8d90edfc15d956eaea6398f81f0214161
kol2104 <kni2104kni@mail.ru>
2024-04-01 01:45:53 +0500

    Update facade

--- src/main/java/com/epam/gymcrm/facade/GymFacade.java
+++ src/main/java/com/epam/gymcrm/facade/GymFacade.java
@@ -1,71 +1,133 @@
 package com.epam.gymcrm.facade;
 
+import com.epam.gymcrm.auth.annotation.Authenticated;
+import com.epam.gymcrm.auth.Authentication;
+import com.epam.gymcrm.model.Role;
 import com.epam.gymcrm.model.Trainee;
 import com.epam.gymcrm.model.Trainer;
 import com.epam.gymcrm.model.Training;
+import com.epam.gymcrm.model.TrainingCriteria;
 import com.epam.gymcrm.service.TraineeService;
 import com.epam.gymcrm.service.TrainerService;
 import com.epam.gymcrm.service.TrainingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 
 @Component
+@RequiredArgsConstructor
 public class GymFacade {
 
     private final TraineeService traineeService;
     private final TrainerService trainerService;
     private final TrainingService trainingService;
 
-    public GymFacade(TraineeService traineeService, TrainerService trainerService, TrainingService trainingService) {
-        this.traineeService = traineeService;
-        this.trainerService = trainerService;
-        this.trainingService = trainingService;
+    private final Authentication authentication;
+
+    public boolean authenticateTrainee(String username, String password) {
+        if (traineeService.isUsernameAndPasswordValid(username, password)) {
+            authentication.setRole(Role.TRAINEE);
+            return true;
+        }
+        return false;
     }
 
-    public List<Trainer> getTrainers() {
-        return trainerService.getAll();
+    public boolean authenticateTrainer(String username, String password) {
+        if (trainerService.isUsernameAndPasswordValid(username, password)) {
+            authentication.setRole(Role.TRAINER);
+            return true;
+        }
+        return false;
     }
 
-    public List<Trainee> getTrainees() {
-        return traineeService.getAll();
-    }
-
-    public List<Training> getTrainings() {
-        return trainingService.getAll();
-    }
-
-
-    public Trainee createTrainee(Trainee trainee) {
-        return traineeService.create(trainee);
-    }
-
-    public Trainer createTrainer(Trainer trainer) {
+    public Trainer createTrainerProfile(Trainer trainer) {
         return trainerService.create(trainer);
     }
 
-    public Training createTraining(Training training) {
+    public Trainee createTraineeProfile(Trainee trainee) {
+        return traineeService.create(trainee);
+    }
+
+    @Authenticated(roles = {Role.TRAINEE, Role.TRAINER})
+    public Trainer getTrainerByUsername(String username) {
+        return trainerService.getByUsername(username);
+    }
+
+    @Authenticated(roles = {Role.TRAINEE, Role.TRAINER})
+    public Trainee getTraineeByUsername(String username) {
+        return traineeService.getByUsername(username);
+    }
+
+    @Authenticated(roles = {Role.TRAINEE})
+    public void changeTraineePassword(Long id, String newPassword) {
+        traineeService.updatePassword(id, newPassword);
+    }
+
+    @Authenticated(roles = {Role.TRAINER})
+    public void changeTrainerPassword(Long id, String newPassword) {
+        trainerService.updatePassword(id, newPassword);
+    }
+
+    @Authenticated(roles = {Role.TRAINER})
+    public Trainer updateTrainerProfile(Long id, Trainer updatedTrainer) {
+        return trainerService.update(id, updatedTrainer);
+    }
+
+    @Authenticated(roles = {Role.TRAINEE})
+    public Trainee updateTraineeProfile(Long id, Trainee updatedTrainee) {
+        return traineeService.update(id, updatedTrainee);
+    }
+
+    @Authenticated(roles = {Role.TRAINEE})
+    public void activateTrainee(Long id) {
+        traineeService.activate(id);
+    }
+
+    @Authenticated(roles = {Role.TRAINEE})
+    public void deactivateTrainee(Long id) {
+        traineeService.deactivate(id);
+    }
+
+
+    @Authenticated(roles = {Role.TRAINER})
+    public void activateTrainer(Long id) {
+        trainerService.activate(id);
+    }
+
+    @Authenticated(roles = {Role.TRAINER})
+    public void deactivateTrainer(Long id) {
+        trainerService.deactivate(id);
+    }
+
+    @Authenticated(roles = {Role.TRAINEE})
+    public void deleteTraineeProfile(String username) {
+        traineeService.deleteByUsername(username);
+    }
+
+    @Authenticated(roles = {Role.TRAINEE, Role.TRAINER})
+    public List<Training> getTraineeTrainings(String traineeUsername, Map<TrainingCriteria, Object> criteria) {
+        return trainingService.getTrainingByTraineeUsernameAndCriteria(traineeUsername, criteria);
+    }
+
+    @Authenticated(roles = {Role.TRAINEE, Role.TRAINER})
+    public List<Training> getTrainerTrainings(String trainerUsername, Map<TrainingCriteria, Object> criteria) {
+        return trainingService.getTrainingByTrainerUsernameAndCriteria(trainerUsername, criteria);
+    }
+
+    @Authenticated(roles = {Role.TRAINEE, Role.TRAINER})
+    public Training addTraining(Training training) {
         return trainingService.create(training);
     }
 
-    public void deleteTrainee(Long id) {
-        traineeService.delete(id);
+    @Authenticated(roles = {Role.TRAINEE, Role.TRAINER})
+    public List<Trainer> getUnassignedTrainers(String traineeUsername) {
+        return traineeService.getUnassignedOnTraineeTrainerListByUsername(traineeUsername);
     }
 
-    public Trainee updateTrainee(Long id, Trainee trainee) {
-        return traineeService.update(id, trainee);
+    @Authenticated(roles = {Role.TRAINEE, Role.TRAINER})
+    public void updateTraineeTrainersList(Long id, List<Trainer> trainers) {
+        traineeService.updateTrainersList(id, trainers);
     }
-
-    public Trainer updateTrainer(Long id, Trainer trainer) {
-        return trainerService.update(id, trainer);
-    }
-
-    public Trainee getTraineeById(Long id) {
-        return traineeService.getById(id);
-    }
-
-    public Trainer getTrainerById(Long id) {
-        return trainerService.getById(id);
-    }
-
 }
