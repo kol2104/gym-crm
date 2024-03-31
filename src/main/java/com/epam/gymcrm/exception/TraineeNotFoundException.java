da7abb4062cb2e33bb0ec6694d64cb650f0298a4
kol2104 <kni2104kni@mail.ru>
2024-04-01 01:45:14 +0500

    Update exceptions

--- src/main/java/com/epam/gymcrm/exception/TraineeNotFoundException.java
+++ src/main/java/com/epam/gymcrm/exception/TraineeNotFoundException.java
@@ -4,4 +4,12 @@ public class TraineeNotFoundException extends RuntimeException {
     public TraineeNotFoundException(Long id) {
         super("Trainee with id '" + id + "' not found");
     }
+
+    public TraineeNotFoundException(String username) {
+        super("Trainee with username '" + username + "' not found");
+    }
+
+    public TraineeNotFoundException(String username, String password) {
+        super("Trainee with username '" + username + "' and password '" + password + "' not found");
+    }
 }
--- src/main/java/com/epam/gymcrm/exception/TrainerNotFoundException.java
+++ src/main/java/com/epam/gymcrm/exception/TrainerNotFoundException.java
@@ -4,4 +4,12 @@ public class TrainerNotFoundException extends RuntimeException {
     public TrainerNotFoundException(Long id) {
         super("Trainer with id '" + id + "' not found");
     }
+
+    public TrainerNotFoundException(String username) {
+        super("Trainer with username '" + username + "' not found");
+    }
+
+    public TrainerNotFoundException(String username, String password) {
+        super("Trainer with username '" + username + "' and password '" + password + "' not found");
+    }
 }
