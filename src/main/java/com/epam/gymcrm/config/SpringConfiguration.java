0e02ede7ef12e0e3555a02679acf6ca5130b2c00
kol2104 <kni2104kni@mail.ru>
2024-04-08 00:59:20 +0500

    Update config to enable web

--- src/main/java/com/epam/gymcrm/config/SpringConfiguration.java
+++ src/main/java/com/epam/gymcrm/config/SpringConfiguration.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableAspectJAutoProxy
 @EnableTransactionManagement
+@EnableWebMvc
 @ComponentScan(basePackages = "com.epam.gymcrm")
 public class SpringConfiguration {
 }
