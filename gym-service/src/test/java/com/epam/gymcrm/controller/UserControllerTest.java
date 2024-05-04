4af8c6846cf4e6f71f2c9ec1e0f96ba7367e03d9
kol2104 <kni2104kni@mail.ru>
2024-05-05 03:24:42 +0500

    Update test

--- gym-service/src/test/java/com/epam/gymcrm/controller/UserControllerTest.java
+++ gym-service/src/test/java/com/epam/gymcrm/controller/UserControllerTest.java
@@ -1,40 +1,25 @@
 package com.epam.gymcrm.controller;
 
-import com.epam.gymcrm.GymCrmApplication;
-import com.epam.gymcrm.auth.JwtAuthenticationFilter;
-import com.epam.gymcrm.common.Constants;
-import com.epam.gymcrm.config.SpringSecurityConfiguration;
-import com.epam.gymcrm.controller.handler.GymCrmExceptionHandler;
 import com.epam.gymcrm.dto.user.UserNewPasswordRequestDto;
 import com.epam.gymcrm.exception.UserNotFoundException;
 import com.epam.gymcrm.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.catalina.security.SecurityConfig;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.security.Principal;
-
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
